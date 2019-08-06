931125be8c62826641c7dcc65aac7216541ac048
Sergio <smartin88@gatech.edu>
2019-08-05 20:55:40 -0500

    saving progress

--- scripts/delete-stack.sh
+++ scripts/delete-stack.sh
@@ -2,6 +2,6 @@ aws cloudformation delete-stack \
 --stack-name udacity-nano-project-2-network \
 --region=us-west-2
 
-aws cloudformation delete-stack \
---stack-name udacity-nano-project-2-infrastructure \
---region=us-west-2
+# aws cloudformation delete-stack \
+# --stack-name udacity-nano-project-2-infrastructure \
+# --region=us-west-2
--- scripts/update-stack.sh
+++ scripts/update-stack.sh
@@ -4,8 +4,8 @@ aws cloudformation update-stack \
 --parameters file://./templates/network-params.json \
 --region=us-west-2
 
-aws cloudformation update-stack \
---stack-name udacity-nano-project-2-infrastructure \
---template-body file://./templates/infrastructure.yml \
---parameters file://./templates/infrastructure-params.json \
---region=us-west-2
+# aws cloudformation update-stack \
+# --stack-name udacity-nano-project-2-infrastructure \
+# --template-body file://./templates/infrastructure.yml \
+# --parameters file://./templates/infrastructure-params.json \
+# --region=us-west-2
