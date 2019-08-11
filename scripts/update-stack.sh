602248f3b95749a12e3e8a6721742f464928d392
Sergio <smartin88@gatech.edu>
2019-08-11 09:58:09 -0500

    final config testing

--- scripts/update-stack.sh
+++ scripts/update-stack.sh
@@ -1,5 +1,5 @@
 aws cloudformation update-stack \
---stack-name udacity-nano-project-2-network \
+--stack-name udacity-nano-project-2 \
 --template-body file://./templates/network-infra.yml \
 --parameters file://./templates/network-infra-params.json \
 --capabilities CAPABILITY_NAMED_IAM \
