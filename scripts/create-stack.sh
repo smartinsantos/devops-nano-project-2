c65fb8f46abc6c9bebbd9a759943a67533faad22
Sergio Martin <smartinsantos@users.noreply.github.com>
2019-08-11 14:01:31 -0500

    Feature/network template (#1)
    * merged feature branch

---
+++ scripts/create-stack.sh
@@ -0,0 +1,6 @@
+aws cloudformation create-stack \
+--stack-name udacity-nano-project-2 \
+--template-body file://./templates/network-infra.yml \
+--parameters file://./templates/network-infra-params.json \
+--capabilities CAPABILITY_NAMED_IAM \
+--region=us-west-2
---
+++ scripts/delete-stack.sh
@@ -0,0 +1,3 @@
+aws cloudformation delete-stack \
+--stack-name udacity-nano-project-2 \
+--region=us-west-2
