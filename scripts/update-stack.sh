f4190bdd68c1339cdcde09db7d9da61d73c7cf28
Sergio <smartin88@gatech.edu>
2019-08-11 08:44:25 -0500

    fixed a bug with malformed cidrip

--- scripts/update-stack.sh
+++ scripts/update-stack.sh
@@ -2,4 +2,5 @@ aws cloudformation update-stack \
 --stack-name udacity-nano-project-2-network \
 --template-body file://./templates/network-infra.yml \
 --parameters file://./templates/network-infra-params.json \
+--capabilities CAPABILITY_NAMED_IAM \
 --region=us-west-2
